4cf07f17d6632fa4ff7611fd47780d3eb1408bba
DanielaGomez <ledangom@gmail.com>
2023-11-02 18:41:31 -0500

    Se hacen modificaciones en el navegador, para que afecte globalmente a la app

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { DetailScreen } from '../screens/DetailScreen';
-; 
+import { Movie } from '../interfaces/movieInterfaces';
 
-const Stack = createNativeStackNavigator();
+
+export type RootStackParams = {
+  HomeScreen: undefined;
+  DetailScreen: Movie; 
+}
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
   return (
       <Stack.Navigator
       screenOptions={{
-        headerShown: false,
-        cardStyle: {
-            backgroundColor: 'white'
-        }
+        headerShown: false
       }}>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="DetailScreen" component={DetailScreen} />
